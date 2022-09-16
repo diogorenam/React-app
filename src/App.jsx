833ecda30fd5276bc610f14ad361d23dd2af5e37
Diogo Rennam <diogorenan006@gmail.com>
2022-09-16 11:47:23 -0300

    version-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,84 @@
 import React, { useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Tasks from './components/Tarefas/Tasks';
-import AddTask from './components/AdicionarTarefas/AddTask';
+import Tasks from "./components/Tarefas/Tasks";
+import AddTask from "./components/AdicionarTarefas/AddTask";
 import Header from "./components/Header/Header";
 
-import './App.css';
+import "./App.css";
 
-const App = ()=>{
-//o metodo useState é usado para poder atualizar uma informação 
- const [tasks,setTasks] = useState([
-  {
-    id: "1",
-    title: "Estudar programação",
-    completed: false,
-  },
-  {
-    id: "2",
-    title: "Academia",
-    completed: true,
-  },
-  {
-    id: "3",
-    title: "Trabalho",
-    completed: true,
-  },
-  
- ]);
- //Adicionar uma nova task 
- const handleTaskAdition = (taskTitle) =>{
-    const newTasks = [...tasks,{
-      title: taskTitle,
-      id: uuidv4(),
+const App = () => {
+  //o metodo useState é usado para poder atualizar uma informação
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar programação",
       completed: false,
-    }]
+    },
+    {
+      id: "2",
+      title: "Academia",
+      completed: true,
+    },
+    {
+      id: "3",
+      title: "Trabalho",
+      completed: true,
+    },
+  ]);
+  //Adicionar uma nova task
+  const handleTaskAdition = (taskTitle) => {
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
     setTasks(newTasks);
- }
+  };
 
- // remove task 
- const handleRemoveTask = (taskId) =>{
-  const newTasks = tasks.filter(task => task.id !== taskId)
+  // remove task
+  const handleRemoveTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
- }
-   
-  
- //Marcar com verde as taks concluidas 
- const handleTaskClick = (taskId) =>{
+  };
+
+  //Marcar com verde as taks concluidas
+  const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
-      if(task.id === taskId) return {...task, completed: !task.completed};
+      if (task.id === taskId) return { ...task, completed: !task.completed };
       return task;
     });
     setTasks(newTasks);
-   
- }
- //retornar as tasks
+  };
+  //retornar as tasks
   return (
-    <>
+    <Router>
+     
       <div className="container">
         <Header/>
-        <AddTask handleTaskAdition = {handleTaskAdition} />
-      <Tasks tasks={tasks} handleTaskClick ={handleTaskClick} handleRemoveTask={handleRemoveTask}  />
-    </div>
-   
-    </>
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAdition={handleTaskAdition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleRemoveTask={handleRemoveTask}
+              />
+            </>
+          )}
 
-  ) 
-  
-}
+        />
+       
+      </div>
+    </Router>
+  );
+};
 
 export default App;
